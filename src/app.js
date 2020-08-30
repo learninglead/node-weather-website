eecded2400b97796228ff0046d5fc9ee9c4584c6
learninglead <learninglead.ooo@gmail.com>
2020-08-30 21:36:22 +0530

    Update details

--- src/app.js
+++ src/app.js
@@ -23,14 +23,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'ABC XYZ'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'ABC XYZ'
     })
 })
 
@@ -38,7 +38,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'ABC XYZ'
     })
 })
 
@@ -73,7 +73,7 @@ app.get('/weather', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'ABC XYZ',
         errorMessage: 'Help article not found.'
     })
 })
@@ -81,7 +81,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'ABC XYZ',
         errorMessage: 'Page not found.'
     })
 })
